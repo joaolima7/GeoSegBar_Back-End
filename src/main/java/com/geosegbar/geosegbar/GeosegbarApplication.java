640e18d5e9c45e4331508659baac7fa7777a25cb
joaolima7 <jcgam3r69@gmail.com>
2025-02-17 20:41:36 -0300

    initialize Spring Boot project with basic configuration and test setup

---
+++ src/main/java/com/geosegbar/geosegbar/GeosegbarApplication.java
@@ -0,0 +1,13 @@
+package com.geosegbar.geosegbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeosegbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeosegbarApplication.class, args);
+	}
+
+}

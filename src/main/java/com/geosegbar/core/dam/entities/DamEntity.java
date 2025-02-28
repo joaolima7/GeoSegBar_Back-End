eacc27190c5d905a206eb38a14e4d6588a26fdac
joaolima7 <jcgam3r69@gmail.com>
2025-02-28 17:38:35 -0300

    add Client and Dam entity relationships: restore ClientEntity class and establish ManyToOne relationship in DamEntity

--- src/main/java/com/geosegbar/core/dam/entities/DamEntity.java
+++ src/main/java/com/geosegbar/core/dam/entities/DamEntity.java
@@ -1,10 +1,14 @@
 package com.geosegbar.core.dam.entities;
 
+import com.geosegbar.core.client.entities.ClientEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -41,4 +45,8 @@ public class DamEntity {
     @Pattern(regexp = "^[A-Za-zÀ-ÿ\\s]+$", message = "O campo não pode conter números!")
     @Size(min = 3, max = 3, message = "A sigla deve ter 3 caracteres!")
     private String acronym;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private ClientEntity client;
 }

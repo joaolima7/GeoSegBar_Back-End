ee65ae9be7047c4a0cb1bad7a19bb19a9d8da195
joaolima7 <jcgam3r69@gmail.com>
2025-02-28 21:51:14 -0300

    remove unused validation interfaces: delete ExistsByAcronymValidation and ExistsByNameValidation

--- src/main/java/com/geosegbar/adapters/validations/ExistsByAcronymValidation.java
+++
@@ -1,6 +0,0 @@
-package com.geosegbar.adapters.validations;
-
-public interface ExistsByAcronymValidation {
-    boolean existsByAcronym(String acronym);
-    boolean existsByAcronymAndIdNot(String acronym, Long id);
-}
--- src/main/java/com/geosegbar/adapters/validations/ExistsByNameValidation.java
+++
@@ -1,6 +0,0 @@
-package com.geosegbar.adapters.validations;
-
-public interface ExistsByNameValidation {
-    boolean existsByName(String name);
-    boolean existsByNameAndIdNot(String name, Long id);
-}

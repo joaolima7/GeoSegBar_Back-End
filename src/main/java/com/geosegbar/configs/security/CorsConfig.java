4eef75a68531f1944ccf0ddaf9889cf3fabf61b4
joaolima7 <jcgam3r69@gmail.com>
2025-05-30 10:04:31 -0300

    fix: Update CORS configuration to allow all origins

--- src/main/java/com/geosegbar/configs/security/CorsConfig.java
+++ src/main/java/com/geosegbar/configs/security/CorsConfig.java
@@ -10,9 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://geometrisa-prod.com.br")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }

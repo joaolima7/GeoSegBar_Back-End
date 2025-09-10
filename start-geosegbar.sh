34fb408666d104d5bc4c3a4a7c4221728daa57dc
joaolima7 <jcgam3r69@gmail.com>
2025-09-10 10:16:13 -0300

    feat: Add missing cd command to start script for correct execution context

--- start-geosegbar.sh
+++ start-geosegbar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /home/wwgeomprod/backend.geometrisa-prod.com.br/target
+
 JAVA_OPTS="-XX:MetaspaceSize=384M -XX:MaxMetaspaceSize=768M -Xms768m -Xmx1536m -XX:+UseG1GC -XX:MaxGCPauseMillis=200"
 
 nohup java $JAVA_OPTS -jar geosegbar-0.0.1-SNAPSHOT.jar --server.port=9090 > log.out 2>&1 &

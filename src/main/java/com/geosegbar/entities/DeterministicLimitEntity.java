a58a1031e80a8b7e9c5c68c5f8b15a8081795867
joaolima7 <jcgam3r69@gmail.com>
2026-02-01 15:08:59 -0300

    feat: Add graph customizations relationship to DeterministicLimitEntity and StatisticalLimitEntity

--- src/main/java/com/geosegbar/entities/DeterministicLimitEntity.java
+++ src/main/java/com/geosegbar/entities/DeterministicLimitEntity.java
@@ -1,15 +1,20 @@
 package com.geosegbar.entities;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -42,4 +47,8 @@ public class DeterministicLimitEntity {
     @JoinColumn(name = "output_id", nullable = false)
     @JsonIgnoreProperties("deterministicLimit")
     private OutputEntity output;
+
+    @OneToMany(mappedBy = "deterministicLimit", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonIgnoreProperties("deterministicLimit")
+    private Set<InstrumentGraphCustomizationPropertiesEntity> graphCustomizations = new HashSet<>();
 }
--- src/main/java/com/geosegbar/entities/StatisticalLimitEntity.java
+++ src/main/java/com/geosegbar/entities/StatisticalLimitEntity.java
@@ -1,15 +1,20 @@
 package com.geosegbar.entities;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -39,4 +44,8 @@ public class StatisticalLimitEntity {
     @JoinColumn(name = "output_id", nullable = false)
     @JsonIgnoreProperties("statisticalLimit")
     private OutputEntity output;
+
+    @OneToMany(mappedBy = "statisticalLimit", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonIgnoreProperties("statisticalLimit")
+    private Set<InstrumentGraphCustomizationPropertiesEntity> graphCustomizations = new HashSet<>();
 }
